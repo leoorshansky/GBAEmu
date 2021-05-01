c65b571276878763dc37fbfe77124b69ff35dabb
Rahul Menon <menonrahul02@gmail.com>
2021-05-01 12:44:41 -0500

    envelope and sweep

--- src/audio/apu.rs
+++ src/audio/apu.rs
@@ -1,3 +1,6 @@
+use rodio::Sample;
+use rodio::OutputStreamHandle;
+use rodio::source::TakeDuration;
 use crate::audio::pulse_wave::PulseWave;
 use rodio::source::Amplify;
 use std::time::Duration;
@@ -22,32 +25,60 @@ const REG_SNDSTAT: u64 = SOUND_OFFSET + 0x84;
 const REG_SNDBIAS: u64 = SOUND_OFFSET + 0x88;
 
 
-// todo: sweep stuff, envelope stuff, channel 3 sample reading, channel 4 noise generation
-// todo: direct sound stuff
-fn play_pulse_wave(duration: Duration, amplitude: f32, duty_cycle: f32, frequency: u32) {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    sink.append(PulseWave::new(frequency as f32, duty_cycle).amplify(amplitude).take_duration(duration));
-
-    // sink.detach();
-    sink.sleep_until_end();
+pub struct APU<'a> {
+    handle: &'a OutputStreamHandle,
+    c1: Sink,
+    c2: Sink,
+    c3: Sink,
+    c4: Sink,
+    d1: Sink,
+    d2: Sink,
 }
 
-fn freq_from_rate(rate: u16) -> u32 {
-    (1 << 17) / (2048 - rate) as u32
+impl<'a> APU<'a> {
+    pub fn new(handle: &'a OutputStreamHandle) -> APU<'a> {
+        let c1 = Sink::try_new(handle).unwrap();
+        let c2 = Sink::try_new(handle).unwrap();
+        let c3 = Sink::try_new(handle).unwrap();
+        let c4 = Sink::try_new(handle).unwrap();
+        let d1 = Sink::try_new(handle).unwrap();
+        let d2 = Sink::try_new(handle).unwrap();
+        APU { handle, c1, c2, c3, c4, d1, d2 }
+    }
+
+    // todo: channel 3 sample reading, channel 4 noise generation
+    // todo: direct sound stuff
+    pub fn play_pulse_wave(&self, duration: Duration, amplitude: f32, duty_cycle: f32, 
+        frequency: u32, env_step: u8, env_dir: bool, env_init: u8, swp_shift: u8, 
+        swp_time: u8, swp_dir: bool) 
+    {
+        let wave = PulseWave::new(frequency as f32, duty_cycle, env_step, env_dir, env_init, swp_shift, swp_time, swp_dir);
+        let wave = wave.take_duration(duration).amplify(amplitude);
+
+        self.c1.append(wave);
+    }
+
+    pub fn freq_from_rate(rate: u16) -> f32 {
+        (1 << 17) as f32 / (2048 - rate) as f32
+    }
+
+    pub fn rate_from_freq(freq: f32) -> u16 {
+        (2048.0 - (1 << 17) as f32 / freq) as u16
+    }
 }
+
+
 
 pub fn make_a_sound() {
-    // play_pulse_wave(Duration::from_secs_f32(0.5), 1.0, 0.5, 440);
-    // play_pulse_wave(Duration::from_secs_f32(0.5), 0.5, 0.5, 440);
-    // play_pulse_wave(Duration::from_secs_f32(0.5), 0.25, 0.5, 440);
-    play_pulse_wave(Duration::from_secs_f32(0.3), 1.0, 0.5, 330);
-    play_pulse_wave(Duration::from_secs_f32(0.2), 1.0, 0.5, 246);
-    play_pulse_wave(Duration::from_secs_f32(0.2), 1.0, 0.5, 261);
-    play_pulse_wave(Duration::from_secs_f32(0.3), 1.0, 0.5, 294);
-    play_pulse_wave(Duration::from_secs_f32(0.2), 1.0, 0.5, 261);
-    play_pulse_wave(Duration::from_secs_f32(0.2), 1.0, 0.5, 246);
-    play_pulse_wave(Duration::from_secs_f32(0.3), 1.0, 0.5, 220);
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let apu = APU::new(&stream_handle);
+    apu.play_pulse_wave(Duration::from_secs_f32(0.3), 0.2, 0.5, 330, 1, true, 16, 0, 1, true);
+    // apu.play_pulse_wave(Duration::from_secs_f32(0.15), 0.2, 0.5, 246, 1, true, 16, 0, 1, true);
+    // apu.play_pulse_wave(Duration::from_secs_f32(0.15), 0.2, 0.5, 261, 1, true, 16, 0, 1, true);
+    // apu.play_pulse_wave(Duration::from_secs_f32(0.3), 0.2, 0.5, 294, 1, true, 16, 0, 1, true);
+    // apu.play_pulse_wave(Duration::from_secs_f32(0.15), 0.2, 0.5, 261, 1, true, 16, 0, 1, true);
+    // apu.play_pulse_wave(Duration::from_secs_f32(0.15), 0.2, 0.5, 246, 1, true, 16, 0, 1, true);
+    // apu.play_pulse_wave(Duration::from_secs_f32(0.3), 0.2, 0.5, 220, 1, true, 16, 0, 1, true);
+    std::thread::sleep(Duration::from_millis(1500));
 }
 
--- src/audio/pulse_wave.rs
+++ src/audio/pulse_wave.rs
@@ -1,18 +1,34 @@
+use crate::audio::apu::APU;
 use std::time::Duration;
 use rodio::Source;
 
+#[derive(Copy, Clone, Debug)]
 pub struct PulseWave {
-    freq: f32,
+    total_cycles: u64,
+    rate: u16,
     duty: f32,
     num_sample: usize,
+    env_step_time: u8,
+    env_dir: bool,
+    env_val: u8,
+    swp_shift: u8,
+    swp_time: u8,
+    swp_dir: bool,
 }
 
 impl PulseWave {
-    pub fn new(freq: f32, duty: f32) -> PulseWave {
+    pub fn new(freq: f32, duty: f32, env_step_time: u8, env_dir: bool, env_init: u8, swp_shift: u8, swp_time: u8, swp_dir: bool) -> PulseWave {
         PulseWave {
-            freq: freq,
+            total_cycles: 0,
+            rate: APU::rate_from_freq(freq),
             duty: duty,
             num_sample: 0,
+            env_step_time: env_step_time,
+            env_dir: env_dir,
+            env_val: env_init,
+            swp_shift: swp_shift,
+            swp_time: swp_time,
+            swp_dir: swp_dir,
         }
     }
 }
@@ -21,9 +37,38 @@ impl Iterator for PulseWave {
     type Item = f32;
 
     fn next(&mut self) -> Option<f32> {
-        self.num_sample = (self.num_sample + 1) % (48000.0 / self.freq) as usize;
+        let freq = APU::freq_from_rate(self.rate);
+        self.total_cycles = self.total_cycles.wrapping_add(1);
+        self.num_sample = (self.num_sample + 1) % (48000.0 / freq) as usize;
 
-        let val = if self.num_sample <= (self.duty * 48000.0 / self.freq) as usize { 1.0 } else { 0.0 };
+        // sweep calculations (374.4 = 7.8 ms * 48kHz)
+        let should_sweep = self.total_cycles % (self.swp_time as f64 * 374.4) as u64 == 0 && (self.swp_shift > 0 && self.swp_time > 0);
+        if should_sweep {
+            let new_rate = self.rate.wrapping_add((if self.swp_dir {65535} else {1} as u16).wrapping_mul(self.rate / (1 << self.swp_shift)));
+            if new_rate > 0 {
+                if new_rate > 2048 {
+                    return None;
+                }
+                self.rate = new_rate;
+                // println!("swept to {}", self.rate);
+            }
+        }
+
+        // envelope calculations (750 = 15.625 ms * 48kHz)
+        let should_bump = self.total_cycles % (self.env_step_time as u64 * 750u64) == 0 && self.total_cycles > 0;
+        if should_bump {
+            self.env_val = self.env_val.wrapping_add(if self.env_dir {1} else {255});
+            self.env_val = std::cmp::min(self.env_val, 16);
+            // println!("bump to {}", self.env_val);
+        }
+        let volume = self.env_val as f32 / 16.0;
+        if should_bump {
+            // println!("{}", volume);
+        }
+
+        // final sound calculations
+        let mut val: f32 = if self.num_sample <= (self.duty * 48000.0 / freq) as usize { 1.0 } else { 0.0 };
+        val *= volume;
         Some(val)
     }
 }
